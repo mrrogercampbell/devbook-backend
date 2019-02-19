78f10831275e1191802a133edee77278032b189f
Roger Campbell II <rc1336@gmail.com>
2019-02-19 15:24:27 -0500

    Setup userSeed.js file. Next step is to add user data to the userSeedData.json file and test to see if I can seed the db.

---
+++ db/userSeed.js
@@ -0,0 +1,18 @@
+const mongoose = require("../Models/UserModel");
+const seeds = require("./userSeedData.json");
+const User = mongoose.model("userModel");
+
+mongoose.Promise = Promise;
+
+User.remove({}).then(_ => {
+  console.log("Dropped the DB");
+  User.collection
+    .insert(seeds)
+    .then(newUser => {
+      console.log(seeds);
+      mongoose.connection.close();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
