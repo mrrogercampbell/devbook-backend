0511a005d4563db535718a42300b06d1106a2bda
Roger Campbell II <rc1336@gmail.com>
2019-02-19 12:11:23 -0500

    Created User Route shell. Might need to update based upon Controller Set up.

---
+++ Routes/UserRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userControllers");
+
+router.get("/", userController.index);
+
+router.get("/new", userController.new);
+
+router.post("/", userController.create);
+
+router.get("/success", userController.success);
+
+router.get("/:id", userController.showOne);
+
+router.delete("/:id", userController.destroy);
+
+router.get("/:id/edit", userController.edit);
+
+router.put("/:id", userController.update);
+
+module.exports = router;
