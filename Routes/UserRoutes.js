d02ac75d275a79236fbd02cd66279df3c527dce2
Roger Campbell II <rc1336@gmail.com>
2019-02-19 16:10:45 -0500

    Able to seed db.

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -4,18 +4,18 @@ const userController = require("../controllers/userControllers");
 
 router.post("/", userController.create);
 
-router.delete("/:id", userController.destroyProfile);
+// router.delete("/:id", userController.destroyProfile);
 
-router.get("/:id/edit", userController.editProfile);
+// router.get("/:id/edit", userController.editProfile);
 
-router.get("/new", userController.new);
+// router.get("/new", userController.new);
 
-router.get("/:id", userController.profile);
+// router.get("/:id", userController.profile);
 
-router.get("/results", userController.searchResults);
+// router.get("/results", userController.searchResults);
 
-router.get("/success", userController.success);
+// router.get("/success", userController.success);
 
-router.put("/:id", userController.updateProfile);
+// router.put("/:id", userController.updateProfile);
 
 module.exports = router;
