c17e5715e2a6d7e6f727562c81ab4b35adec5326
Roger Campbell II <rc1336@gmail.com>
2019-02-20 01:57:10 -0500

    Updated job route info in appRoutes.js like 5. Old: '/jobsRoute' Current: '/JobRoutes.'

--- Routes/appRoutes.js
+++ Routes/appRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/user", require("./UserRoutes"));
-// router.use('/job', require("./jobsRoutes"))
+router.use('/job', require("./JobRoutes"))
 router.use('/projects', require("./ProjectRoutes"))
 router.use('/posts', require("./PostRoutes"))
 // router.use('/job', require("./commentsRoutes"))
