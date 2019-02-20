44b71e7b3834ddf3b3421fe17a0d17a637b182ed
Roger Campbell II <rc1336@gmail.com>
2019-02-20 02:07:05 -0500

    Had to fix type in JobRoutes.js line 3. Old: '../Controllers/JobController, New: ''../Controller/jobController.'

--- Routes/JobRoutes.js
+++ Routes/JobRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const JobController = require("../Controllers/JobControllers");
+const JobController = require("../Controllers/jobController");
 
 router.get("/", JobController.index);
 
