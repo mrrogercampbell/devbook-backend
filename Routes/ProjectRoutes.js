df9bad39311aa98f9e2228cb75e6db57539e6ced
Roger Campbell II <rc1336@gmail.com>
2019-02-19 22:17:39 -0500

    Built out skelaten of Project Routes. Next step dummy Controllers.

---
+++ Routes/ProjectRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const ProjectController = require("../Controllers/projectControllers");
+
+router.get("/", ProjectController.index);
+
+router.post("/", ProjectController.create);
+
+// router.delete("/:id", ProjectController.destroyProfile);
+
+// router.get("/:id/edit", ProjectController.editProfile);
+
+// router.get("/new", ProjectController.new);
+
+router.get("/:id", ProjectController.profile);
+
+// router.get("/results", ProjectController.searchResults);
+
+// router.get("/success", ProjectController.success);
+
+// router.put("/:id", ProjectController.updateProfile);
+
+module.exports = router;
