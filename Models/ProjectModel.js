f4f091b4fd885cd452dc97b0b1720b9f784d25bc
Roger Campbell II <rc1336@gmail.com>
2019-02-19 22:12:52 -0500

    Create ProjectModel.js. Built out Schema. Next step layout Routes, then on to Controllers.

---
+++ Models/ProjectModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
+
+const Project = new Schema({
+  content: String,
+  position: String,
+  company: String,
+  location: String,
+  companyLogo: String,
+  createdAt: {
+    Type: Date,
+    Default: Date.now()
+  }
+  // user:
+  // userId: [Linked Model ]
+  // userName: [Linked Model]
+});
+
+mongoose.model("ProjectModel", Project);
+
+module.exports = mongoose;
---
+++ READ.md
@@ -0,0 +1,9 @@
+ToDo List:
+
+## Project RMC
+
+    1) Model
+    2) Routes
+    3) Controllers
+    4) DB config
+    5) SeedData/Test
