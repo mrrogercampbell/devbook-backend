e39c8f692f4945c75169d999a20d6752af28e6d0
Roger Campbell II <rc1336@gmail.com>
2019-02-19 23:06:57 -0500

    Built out PostControllers.js. Net Stop: Controllers, Then: DB Config.

---
+++ Routes/PostRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const PostController = require("../Controllers/PostControllers");
+
+router.get("/", PostController.index);
+
+router.post("/", PostController.create);
+
+// router.delete("/:id", PostController.destroyProfile);
+
+// router.get("/:id/edit", PostController.editProfile);
+
+// router.get("/new", PostController.new);
+
+router.get("/:id", PostController.profile);
+
+// router.get("/results", PostController.searchResults);
+
+// router.get("/success", PostController.success);
+
+// router.put("/:id", PostController.updateProfile);
+
+module.exports = router;
