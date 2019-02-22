65e5e3614a8fe880ea2b9ebf07450839c48166eb
Mifta Hamid <miftahamid@Miftas-MacBook-Air.local>
2019-02-22 05:01:56 -0500

    have the functions in passport

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ app.use(cors());
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 app.use(express.static("client/build"));
-
+const passport = require("./config/passport")();
 const routes = require("./Routes/appRoutes");
 
 app.use("/", routes);
+app.use(passport.initialize());
 
 app.set("port", process.env.PORT || 3001);
 
