9d65981e979ddff1faaa3fdd4cf276531fb1f94e
Mifta Hamid <miftahamid@Miftas-MacBook-Air.local>
2019-02-22 05:48:05 -0500

    ok mostly working but i have to look in to the user modele

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ app.use(cors());
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 app.use(express.static("client/build"));
-const passport = require("./config/passport")();
 const routes = require("./Routes/appRoutes");
+const passport = require("./config/passport")();
+const userController = require("./Controllers/users");
 
 app.use("/", routes);
 app.use(passport.initialize());
+app.use("/users", userController);
 
 app.set("port", process.env.PORT || 3001);
 
